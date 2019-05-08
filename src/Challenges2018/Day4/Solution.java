81cc4d82c8ae338e30a68297f43346fdc3c58599
AfaqAnwar <anwarafaq@protonmail.com>
2019-05-07 23:25:23 -0400

    Setup Day4 package. Attempted a solution, but it wasn't working. Will try a different approach later.
    Signed-off-by: AfaqAnwar <AnwarAfaq@protonmail.com>

---
+++ src/Challenges2018/Day4/Solution.java
@@ -0,0 +1,30 @@
+package Challenges2018.Day4;
+
+import Challenges2018.InputReader;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+/**
+ * Solutions to the Day Four Puzzles.
+ * @Author Afaq Anwar
+ * @Version 05/07/2019
+ */
+public class Solution {
+    public static String puzzleOne(ArrayList<String> input) {
+        return "";
+    }
+
+    public static String puzzleTwo() {
+        return "";
+    }
+
+    public static void main(String[] args) throws IOException {
+        InputReader inputReader = new InputReader(new File("src/Challenges2018/Day4/input.txt"));
+        System.out.println("First Puzzle Answer - " + puzzleOne(inputReader.getInput()));
+        System.out.println("Second Puzzle Answer - " + puzzleTwo());
+    }
+}
