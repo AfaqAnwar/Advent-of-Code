be689747fe6d0e0aaa81153ec71aa74f752bfb37
AfaqAnwar <anwarafaq@protonmail.com>
2019-05-14 01:03:34 -0400

    Puzzle One needs to be debugged. Testing with the example from the website. Also spell check Guard.
    Signed-off-by: AfaqAnwar <AnwarAfaq@protonmail.com>

--- src/Challenges2018/Day4/Guard.java
+++ src/Challenges2018/Day4/Guard.java
@@ -9,28 +9,33 @@ import java.util.HashMap;
  * @Author Afaq Anwar
  * @Version 05/14/2019
  */
-public class Gaurd {
+public class Guard {
     private int id;
     private HashMap<String, int[]> scheduleLog;
     private int[] minutes;
     private int mostFrequent;
 
-    public Gaurd(int id) {
+    public Guard(int id) {
         this.id = id;
         scheduleLog = new HashMap<>();
         minutes = new int[60];
+        mostFrequent = 0;
     }
+
+    public int getId() { return this.id; }
+
+    public HashMap<String, int[]> getScheduleLog() { return this.scheduleLog; }
 
     public void updateMinutes(ArrayList<String> log) {
         int start = 0;
         int end = 0;
-        int duration = 0;
         for (String entry : log) {
-            entry.replace("[", "").replace("]", "");
-            String[] splitStr = entry.split(" ");
+            String altered = entry.replace("[", "").replace("]", "");
+            String[] splitStr = altered.split(" ");
             String date = splitStr[0];
             String status = splitStr[2];
             if (!scheduleLog.containsKey(date)) {
+                minutes = new int[60];
                 if (status.equals("falls")) {
                     start = Integer.parseInt(splitStr[1].split(":")[1]);
                 } else if (status.equals("wakes")) {
@@ -54,5 +59,42 @@ public class Gaurd {
        for (int i = start; i < end; i++) {
            minutes[i] = 1;
        }
+    }
+
+    public int calculateTotalSleep() {
+        int total = 0;
+        for (int[] log : scheduleLog.values()) {
+            for (int i : log) {
+                if (i == 1) {
+                    total++;
+                }
+            }
+        }
+        return total;
+    }
+
+    public int getMostFrequentMinute() {
+        HashMap<Integer, Integer> minuteMap = new HashMap<>();
+        for (int[] log : scheduleLog.values()) {
+            for (int i = 0; i < log.length; i++) {
+                if (log[i] == 1) {
+                    if (!minuteMap.containsKey(i)) {
+                        minuteMap.put(i, 1);
+                    } else {
+                        int current = minuteMap.get(i);
+                        current++;
+                        minuteMap.replace(i, current);
+                    }
+                }
+            }
+        }
+        int greatest = 0;
+        for (int key : minuteMap.keySet()) {
+            if (minuteMap.get(key) > greatest) {
+                mostFrequent = key;
+                greatest = minuteMap.get(key);
+            }
+        }
+        return this.mostFrequent;
     }
 }
--- src/Challenges2018/Day4/Solution.java
+++ src/Challenges2018/Day4/Solution.java
@@ -16,32 +16,52 @@ import java.util.HashMap;
 public class Solution {
     public static String puzzleOne(ArrayList<String> input) {
         Collections.sort(input);
-        ArrayList<Gaurd> gaurds = new ArrayList<>();
+        ArrayList<Guard> guards = new ArrayList<>();
         int id = 0;
         int start = 0;
         int end = 0;
-        for (int i = 0; i < input.size(); i++) {
+        for (int i = 0; i < input.size() - 1; i++) {
             String statusKeyword = input.get(i).split(" ")[3];
             if (!statusKeyword.equals("asleep") && !statusKeyword.equals("up")) {
                 int currentID = Integer.parseInt(statusKeyword.replace("#", ""));
                 if (currentID != id) {
+                    id = currentID;
                     start = i + 1;
-                    end = i;
-                    while (input.get(end).split(" ")[3].equals("asleep") || input.get(end).split(" ")[3].equals("up")) {
-                        end++;
+                    end = start;
+                    while (input.get(end).split(" ")[3].equals("asleep") && end < input.size() - 1 || input.get(end).split(" ")[3].equals("up") && end < input.size() - 1) {
+                        if (end < input.size() - 1) {
+                            end++;
+                        }
                     }
-                    Gaurd gaurd = new Gaurd(currentID);
-                    gaurd.updateMinutes(generateLogList(input, start, end));
-                    gaurds.add(gaurd);
+                    Guard guard = new Guard(id);
+                    guard.updateMinutes(generateLogList(input, start, end));
+                    guards.add(guard);
                 }
             }
         }
-        return "";
+        for (Guard guard : guards) {
+            for (String date : guard.getScheduleLog().keySet()) {
+                System.out.println(guard.getId() + " " + date);
+                for (int i : guard.getScheduleLog().get(date)) {
+                    System.out.print(i + ",");
+                }
+            }
+        }
+        Guard chosen = null;
+        int mostMinutes = 0;
+        for (Guard guard : guards) {
+            int totalSleep = guard.calculateTotalSleep();
+            if (totalSleep > mostMinutes) {
+                chosen = guard;
+                mostMinutes = guard.calculateTotalSleep();
+            }
+        }
+        return Integer.toString(chosen.getMostFrequentMinute() * chosen.getId());
     }
 
     public static ArrayList<String> generateLogList(ArrayList<String> input, int start, int end) {
         ArrayList<String> log = new ArrayList<>();
-        for (int i = start; i <= end; i++) {
+        for (int i = start; i < end; i++) {
             log.add(input.get(i));
         }
         return log;
@@ -52,7 +72,7 @@ public class Solution {
     }
 
     public static void main(String[] args) throws IOException {
-        InputReader inputReader = new InputReader(new File("src/Challenges2018/Day4/input.txt"));
+        InputReader inputReader = new InputReader(new File("src/Challenges2018/Day4/text.txt"));
         System.out.println("First Puzzle Answer - " + puzzleOne(inputReader.getInput()));
         System.out.println("Second Puzzle Answer - " + puzzleTwo());
     }
