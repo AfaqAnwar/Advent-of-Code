ee13f022d030d2719f01edb86122562d6cc6dab2
AfaqAnwar <anwarafaq@protonmail.com>
2019-05-20 19:59:05 -0400

    Finished Day 5.
    Signed-off-by: AfaqAnwar <AnwarAfaq@protonmail.com>

--- src/Challenges2018/Day5/Solution.java
+++ src/Challenges2018/Day5/Solution.java
@@ -6,15 +6,23 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * Solutions to the Day Five Puzzles.
  * @Author Afaq Anwar
- * @Version 05/19/2019
+ * @Version 05/20/2019
  */
 public class Solution {
-    public static String puzzleOne(ArrayList<String> input) {
-        String[] units = input.get(0).split("");
+    private static String[] alphabet = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+    private static HashMap<String, Integer> alphabetMap = new HashMap<>();
+
+    /**
+     * Breaks down the split units.
+     * @param units String of units.
+     * @return Integer that represents the length of the polymer after all possible breakdowns have occurred.
+     */
+    public static int findPolymerLength(String[] units) {
         boolean destroy = false;
         int originalSize = units.length;
         int currentSize = 0;
@@ -53,7 +61,13 @@ public class Solution {
             units = condense(units);
             currentSize = units.length;
         }
-        return Integer.toString(units.length);
+        return units.length;
+    }
+
+    // Puzzle One. Returns the Length of the original polymer after all breakdowns have occurred.
+    public static String puzzleOne(ArrayList<String> input) {
+        String[] units = input.get(0).split("");
+        return Integer.toString(findPolymerLength(units));
     }
 
     /**
@@ -79,11 +93,36 @@ public class Solution {
         return newArr;
     }
 
-    public static String puzzleTwo() { return ""; }
+    // Puzzle Two. Returns the length of the smallest polymer after one letter has been removed.
+    public static String puzzleTwo(ArrayList<String> input) {
+        // Looping through each letter of the alphabet and removes that letter.
+        for (String letter : alphabet) {
+            String replacedStr = input.get(0).replaceAll(letter, "").replaceAll(letter.toLowerCase(), "");
+            String[] units = replacedStr.split("");
+            int length = findPolymerLength(units);
+            alphabetMap.put(letter, length);
+        }
+        return Integer.toString(findLowest(alphabetMap));
+    }
+
+    /**
+     * Helper Method, finds the smallest Integer value in a valid HashMap.
+     * @param map HashMap that has String keys & Integer Entries.
+     * @return Smallest Integer Value within map.
+     */
+    public static int findLowest(HashMap<String, Integer> map) {
+        int smallestValue = 100000000;
+        for (String entry : map.keySet()) {
+            if (map.get(entry) < smallestValue) {
+                smallestValue = map.get(entry);
+            }
+        }
+        return smallestValue;
+    }
 
     public static void main(String[] args) throws IOException {
         InputReader inputReader = new InputReader(new File("src/Challenges2018/Day5/input.txt"));
         System.out.println("First Puzzle Answer - " + puzzleOne(inputReader.getInput()));
-        System.out.println("Second Puzzle Answer - " + puzzleTwo());
+        System.out.println("Second Puzzle Answer - " + puzzleTwo(inputReader.getInput()));
     }
 }
