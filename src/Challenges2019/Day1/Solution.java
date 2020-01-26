179e3b2900446ea3097b562ab3ed0a326f05be7c
Afaq Anwar <anwarafaq@protonmail.com>
2020-01-26 18:37:52 -0500

    Finished Day One, 2019.
    Signed-off-by: AfaqAnwar <anwarafaq@protonmail.com>

--- src/Challenges2019/Day1/Solution.java
+++ src/Challenges2019/Day1/Solution.java
@@ -5,6 +5,7 @@ import Utilities.InputReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Solutions to the Day One Puzzles.
@@ -14,19 +15,33 @@ import java.util.ArrayList;
 public class Solution {
     // Finds the amount of fuel needed.
     public static String puzzleOne(ArrayList<String> input) {
-        int total = 0;
-        for (String fuel : input) {
-            int amount = Integer.parseInt(fuel);
+        int totalFuel = 0;
+        for (String fuelMass : input) {
+            int currentAmount = Integer.parseInt(fuelMass);
             // Java rounds down.
-            amount /= 3;
-            amount -= 2;
-            total += amount;
+            currentAmount /= 3;
+            currentAmount -= 2;
+
+            totalFuel += currentAmount;
         }
-        return Integer.toString(total);
+        return Integer.toString(totalFuel);
     }
 
+    // Finds the amount of fuel needed for each module.
     public static String puzzleTwo(ArrayList<String> input) {
-      return "";
+        int totalRequirement = 0;
+        for (String module : input) {
+            int currentModule = Integer.parseInt(module);
+            while (currentModule > 0) {
+                currentModule /= 3;
+                currentModule -= 2;
+                // Checks for last case because the while loop is not concurrent.
+                if (currentModule > 0) {
+                    totalRequirement += currentModule;
+                }
+            }
+        }
+        return Integer.toString(totalRequirement);
     }
 
     public static void main(String[] args) throws IOException {
